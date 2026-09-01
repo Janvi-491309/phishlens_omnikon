b0be8847b562c04199fbb381f65876a5ff112c69
Navya Marivada <navyamarivada1@gmail.com>
2026-09-01 23:22:55 +0530

    Prepare frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://phishlens-omnikon.onrender.com',
+  baseURL: 'https://phishlens-omnikon.onrender.com/api/docs',
   headers: {
     'Content-Type': 'application/json',
   },
